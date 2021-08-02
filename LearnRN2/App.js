fff723f3b9e4d4a38e9b51eba57f1041760fd635
徐岩华 <zhenaihua0213@163.com>
2021-08-02 18:56:56 +0800

    状态机

--- LearnRN2/App.js
+++ LearnRN2/App.js
@@ -6,49 +6,75 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import type { Node } from 'react';
 import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
 
 let widthOfMargin = Dimensions.get('window').width * 0.05;
 
-const App: () => Node = () => {
-  return (
-    <View style={styles.contianer}>
-      <TextInput style={styles.textInputStyle} placeholder={'请输入手机号'} />
-      <Text style={styles.textPromptStyle}>
-        您输入的手机号：
-      </Text>
-      <TextInput style={styles.textInputStyle} placeholder={'请输入密码'} secureTextEntry={true} />
-      <Text style={styles.bigTextPromptStyle}>确定</Text>
-    </View>
-  );
-};
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputNum: '',
+            inputPW: ''
+        };
+        this.updatePW = this.updatePW.bind(this);
+    }
+
+    updateNum(newText) {
+        this.setState((state) => {
+            return {
+                inputNum: newText,
+            }
+        })
+    }
+    updatePW(newText) {
+        this.setState((state) => {
+            return {
+                inputPW: newText,
+            }
+        })
+    }
+    render() {
+        return (
+            <View style={styles.contianer}>
+              <TextInput style={styles.textInputStyle} placeholder={'请输入手机号'} onChangeText={(newText) => this.updateNum(newText)}/>
+              <Text style={styles.textPromptStyle}>
+                您输入的手机号：{this.state.inputNum}
+              </Text>
+              <TextInput style={styles.textInputStyle} placeholder={'请输入密码'} secureTextEntry={true} />
+              <Text style={styles.bigTextPromptStyle}>确定</Text>
+            </View>
+        );
+    }
+}
+;
 
 const styles = StyleSheet.create({
-  contianer: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: 80
-  },
-  textInputStyle: {
-    margin: widthOfMargin,
-    backgroundColor: 'gray',
-    fontSize: 20
-  },
-  textPromptStyle: {
-    margin: widthOfMargin,
-    fontSize: 20
-  },
-  bigTextPromptStyle: {
-    margin: widthOfMargin,
-    backgroundColor: 'gray',
-    fontSize: 20,
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 30,
-    padding: 10
-  }
+    contianer: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 80
+    },
+    textInputStyle: {
+        margin: widthOfMargin,
+        backgroundColor: 'gray',
+        fontSize: 20
+    },
+    textPromptStyle: {
+        margin: widthOfMargin,
+        fontSize: 20
+    },
+    bigTextPromptStyle: {
+        margin: widthOfMargin,
+        backgroundColor: 'gray',
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30,
+        padding: 10
+    }
 });
 
-export default App;
